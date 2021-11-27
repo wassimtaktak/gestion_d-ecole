7c963182b7bde702fdcce2c6838f98521d56eaf6
wassimtaktak <wassimtaktak32@gmail.com>
2021-11-28 00:05:16 +0100

    5

--- src/ecole/eleve.java
+++ src/ecole/eleve.java
@@ -119,6 +119,7 @@ public class eleve extends JFrame
 		});
 //aaaaaaaaaaaaaaaaaaaaaa
 //tttttttttttttt
+//jjjjjjjjjjjjjjjjjjjjjjjj
 		b02 = new JButton();
 		b02.setText("Annuler");
 		b02.setPreferredSize(new Dimension( 100, 25));
